ba059370e6fd059316f786d8cc98e18b33dc964d
Dan Michael O. Heggø <Dan.Michael.Heggo@bibsent.no>
2022-06-06 19:53:37 +0200

    Bump version classifiers

--- setup.py
+++ setup.py
@@ -17,9 +17,7 @@ setup(name='sdbot',
       },
       classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
       ],
       install_requires=[
         'numpy',
